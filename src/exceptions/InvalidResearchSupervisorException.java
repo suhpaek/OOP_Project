dfa9035d06246608fee294642a3a4afe0f9b7e7b
aru <arukaszhen@gmail.com>
2026-05-20 16:09:32 +0500

    final

--- src/exceptions/InvalidResearchSupervisorException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidResearchSupervisorException extends UniversityException {
-    public InvalidResearchSupervisorException(String researcherName, int hIndex) {
-        super("Research supervisor '" + researcherName + "' has h-index " + hIndex + ", but at least 3 is required.");
-    }
-}
